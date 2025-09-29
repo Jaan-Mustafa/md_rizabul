62f61e7c89e495c47d73beca1c66c46eb7fab245
Md Rizabul <mustafamustafa9891@gmail.com>
2025-09-29 19:30:24 +0530

    added header and footer

---
+++ app/footer.tsx
@@ -0,0 +1,15 @@
+export function Footer(){
+    return (
+      <footer className="p-6 pt-3 pb-6 flex text-xs text-center mt-3 dark:text-gray-400 text-gray-500 font-mono">
+        <div className="grow text-left">
+          Md Rizabul (
+          <div>
+            <a href="https://x.com/Jaan_Mustafa_">@Jaan_Mustafa</a>
+          </div>
+          )
+        </div>
+      </footer>
+    )
+
+
+}
